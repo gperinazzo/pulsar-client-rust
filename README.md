b84ef164f4c20d99e1306cb8006a3ae8731e818f
Guilherme Perinazzo <gperinazzo@gmail.com>
2019-09-23 01:32:21 -0300

    Add initial files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pulsar Client for Rust
+
+WIP: Still not usable for anything.
+
+This is a wrapper for the C++/C client library.
---
+++ wrapper.h
@@ -0,0 +1,15 @@
+#include <pulsar/c/authentication.h>
+#include <pulsar/c/client_configuration.h>
+#include <pulsar/c/client.h>
+#include <pulsar/c/consumer_configuration.h>
+#include <pulsar/c/consumer.h>
+#include <pulsar/c/message.h>
+#include <pulsar/c/message_id.h>
+#include <pulsar/c/message_router.h>
+#include <pulsar/c/producer_configuration.h>
+#include <pulsar/c/producer.h>
+#include <pulsar/c/reader_configuration.h>
+#include <pulsar/c/reader.h>
+#include <pulsar/c/result.h>
+#include <pulsar/c/string_list.h>
+#include <pulsar/c/string_map.h>
